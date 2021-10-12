5a84404b24fc9f2986b9f14c911598f73b0553b7
ziomek64 <mradymski3@gmail.com>
2021-10-13 01:10:34 +0200

    Add project files.

---
+++ Converters/Converters/DateTimetoStringConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Converters
+{
+    /// <summary>
+    ///     Converts a DateTime to string value.
+    /// </summary>
+    public class DateTimetoStringConverter : IValueConverter
+    {
+        /// <summary>
+        /// Default format used when parameter is not provided.
+        /// Example string with this format - 10/31/2008 5:04 PM
+        /// </summary>
+        private const string DefaultFormat = "G";
+        
+        /// <summary>
+        /// List of compatible formats.
+        /// See https://docs.microsoft.com/en-us/dotnet/api/system.datetime.tostring?view=net-5.0 for more information.
+        /// </summary>
+        private static readonly string[] Formats = {"d", "D", "f", "F", "g", "G", "m", "o", "r",
+            "s", "t", "T", "u", "U", "Y"};
+        
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">The type of the binding target property. This is not implemented.</param>
+        /// <param name="parameter">Additional parameter for the converter to handle as a format. If not provided, null or empty it is "G" format by default.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not DateTime datetime)
+                throw new ArgumentException("Value is not a valid DateTime.");
+
+            string format;
+
+            string parameterString = parameter?.ToString();
+
+            if (string.IsNullOrEmpty(parameterString))
+            {
+                format = DefaultFormat;
+            }
+            else
+            {
+                if (!(Formats.Contains(parameterString)))
+                    throw new ArgumentException("Format parameter not supported.");
+                
+                format = parameterString;
+            }
+            
+            return datetime.ToString(format, culture);
+        }
+        
+        /// <summary>
+        /// This method is not implemented and will throw a <see cref="NotImplementedException"/>.
+        /// </summary>
+        /// <param name="value">N/A</param>
+        /// <param name="targetType">N/A</param>
+        /// <param name="parameter">N/A</param>
+        /// <param name="culture">N/A</param>
+        /// <returns>N/A</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
---
+++ Converters/Converters/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Converters
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        
+        public DateTime _date = new DateTime(2021, 05, 05, 15, 6, 6);
+        public DateTime date
+        {
+            get => _date;
+            set
+            {
+                if (_date == value)
+                    return;
+
+                _date = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
